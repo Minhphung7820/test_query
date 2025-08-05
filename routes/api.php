74b84a29fe01c8a0dbbd284f1f241f9fa5d9cb5f
Minhphung7820 <110077842+Minhphung7820@users.noreply.github.com>
2025-08-05 16:46:19 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,34 +4,26 @@ use App\Models\Order;
 use App\Models\OrderItem;
 use Illuminate\Support\Facades\Log;
 Route::get('/orders', function () {
-    $startMemory = memory_get_usage(true); // RAM đầu
-    $startTime = microtime(true);          // thời gian bắt đầu
+    $startMemory = memory_get_usage(true);
+    $startTime = microtime(true);
 
-    // --- LOGIC CHÍNH ---
     $orders = Order::limit(10)->get();
-    $orderIds = $orders->pluck('id')->all();
-    $items = OrderItem::whereIn('order_id', $orderIds)
-    ->lazy()->each(function ($item) {
-       Log::info($item);
-    });
-    dd(34);
-    $orders->withChildren([
-        ['id', '=', 'order_id'],
-        ['location_id', '=', 'location_id']
-    ], $items, 'id', 'order_id', 'items');
 
-    // --- TÍNH TOÁN ---
+    $orders = $orders->withChildrenPerParent(
+        childQuery: OrderItem::query(),
+        matchColumns: [['id', '=', 'order_id'], ['location_id', '=', 'location_id']],
+        relationName: 'items',
+        limit: 10
+    );
+
     $endMemory = memory_get_usage(true);
     $endTime = microtime(true);
-
-    $memoryUsed = round(($endMemory - $startMemory) / 1024 / 1024, 2); // MB
-    $executionTime = round(($endTime - $startTime) * 1000, 2); // ms
 
     return response()->json([
         'orders' => $orders,
         'debug' => [
-            'memory_used_mb' => $memoryUsed,
-            'execution_time_ms' => $executionTime
+            'memory_used_mb' => round(($endMemory - $startMemory) / 1024 / 1024, 2),
+            'execution_time_ms' => round(($endTime - $startTime) * 1000, 2)
         ]
     ]);
 });
